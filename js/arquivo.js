c1e4e1f9a55d16d7d610fa366f5e5da4d6978bab
Andre Alves <37510133+AndAllves@users.noreply.github.com>
2022-02-16 14:32:19 -0300

    animação de banner

--- js/arquivo.js
+++ js/arquivo.js
@@ -1,9 +1,21 @@
-function escreverCorpo() {
-    ('#corpo').innerHTML(posta)
-}
 
-window.onclick('')
+$( function() {
+    var limite = $('#janela img').length * $('#janela img').width() * -1;
+    var largura = (limite * (-1)) + $('#janela img').width();
 
+    $('#janela').append('<img>');
+    $('#janela img:last').attr('src', '../imagens/Exame.jpg')
+    $('#janela').css('width', largura)
 
+    function moverBanner() {
+        $('#janela').animate({left:"-=450"}, 1000, function(e) {
+            if($('#janela').position().left <= limite){
+                $('#janela').css('left', 0)
+            }
+        });
+    }
 
+    setInterval(moverBanner, 3000)
+ 
+})
 
